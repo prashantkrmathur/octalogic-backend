4ce6c9581d8ba71886b3ef758cbff3f9a77a5a43
Prashant Kumar <prashantkrmathur@gmail.com>
2025-07-04 22:25:17 +0530

    created module controller and service file for user

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 import typeorm from './config/typeorm';
 
 @Module({
@@ -15,6 +16,7 @@ import typeorm from './config/typeorm';
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => (configService.get('typeorm'))
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
