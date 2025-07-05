2ca17b47806c902baa49874438d2f7c510d5994b
Prashant Kumar <prashantkrmathur@gmail.com>
2025-07-05 16:44:38 +0530

    cors configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
+  const allowedOrigins = ['*'];
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+  app.setGlobalPrefix('api');
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000);
+  
 }
 bootstrap();
