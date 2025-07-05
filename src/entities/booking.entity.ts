e7e28b1256e37b3a5dc38c09651702cdddeaef92
Prashant Kumar <prashantkrmathur@gmail.com>
2025-07-05 22:09:40 +0530

    import fix for deployment

--- src/entities/booking.entity.ts
+++ src/entities/booking.entity.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
-import { User } from './user.entity';
 import { Vehicle } from './vehicle.entity';
+import { User } from './user.entity';
 
 @Entity()
 export class Booking {
--- src/vehicles/vehicles.module.ts
+++ src/vehicles/vehicles.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Booking } from '../entities/booking.entity';
-import { User } from '../entities/user.entity';
 import { Vehicle } from '../entities/vehicle.entity';
 import { VehicleCategory } from '../entities/vehicle_category.entity';
 import { VehicleController } from './vehicles.controller';
+import { User } from 'src/entities/user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Vehicle, VehicleCategory, Booking, User])],
